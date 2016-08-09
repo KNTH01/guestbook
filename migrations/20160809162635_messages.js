54def7415bc1a84765fefe45b07cfc970507446f
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-08-09 16:44:26 +0200

    lint file

--- migrations/20160809162635_messages.js
+++ migrations/20160809162635_messages.js
@@ -1,11 +1,7 @@
 
 const tableName = 'messages'
-/*
-foreigntable.foreign(columns)
-Adds a foreign key constraint to a table for an existing column using table.foreign(column).references(column) or multiple columns using table.foreign(columns).references(columns). You can also chain onDelete and/or onUpdate to set the reference option (RESTRICT, CASCADE, SET NULL, NO ACTION) for the operation. Note, this is the same as column.references(column) but works for existing column
- */
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex.schema.createTable(tableName, function (table) {
     table.increments()
     table.integer('userId').unsigned().index().references('id').inTable('users')
@@ -15,6 +11,6 @@ exports.up = function(knex, Promise) {
   })
 }
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTable(tableName)
 }
