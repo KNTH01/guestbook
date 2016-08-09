8c19eec6f85c0ce746d12e79073b809454f43049
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-08-09 16:38:37 +0200

    add relation to messages with userId

--- migrations/20160809162635_messages.js
+++ migrations/20160809162635_messages.js
@@ -1,9 +1,14 @@
 
 const tableName = 'messages'
+/*
+foreigntable.foreign(columns)
+Adds a foreign key constraint to a table for an existing column using table.foreign(column).references(column) or multiple columns using table.foreign(columns).references(columns). You can also chain onDelete and/or onUpdate to set the reference option (RESTRICT, CASCADE, SET NULL, NO ACTION) for the operation. Note, this is the same as column.references(column) but works for existing column
+ */
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable(tableName, function (table) {
     table.increments()
+    table.integer('userId').unsigned().index().references('id').inTable('users')
     table.text('content')
     table.dateTime('createdAt')
     table.dateTime('updatedAt')
