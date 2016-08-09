3c0b5273bede3862a6d560fce9e0d96c043adca1
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-08-09 13:45:47 +0200

    update migration setup

--- migrations/20160809132654_setup.js
+++ migrations/20160809132654_setup.js
@@ -1,12 +1,12 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments()
-    table.string('email')
+    table.string('email').unique()
     table.string('password')
     table.string('first_name')
     table.string('last_name')
-    table.dateTime('created_at')
-    table.dateTime('updated_at')
+    table.dateTime('created_at').defaultTo(knex.raw('now()'))
+    table.dateTime('updated_at').defaultTo(knex.raw('now()'))
   })
 }
 
