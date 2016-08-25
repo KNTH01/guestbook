9d3a7182ea1ff7a5f48f161f4925d50877e5f1d8
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-08-25 13:19:46 +0200

    edit the migration message, and update the seed order

--- migrations/20160809162635_messages.js
+++ migrations/20160809162635_messages.js
@@ -4,7 +4,8 @@ const tableName = 'messages'
 exports.up = function (knex, Promise) {
   return knex.schema.createTable(tableName, function (table) {
     table.increments()
-    table.integer('userId').unsigned().index().references('id').inTable('users')
+    table.integer('userId').unsigned().index()
+      .references('id').inTable('users').onDelete('SET NULL')
     table.text('content')
     table.dateTime('createdAt')
     table.dateTime('updatedAt')
